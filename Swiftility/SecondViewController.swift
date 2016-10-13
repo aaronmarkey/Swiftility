b9386598790a327de12da42e9952771341bb970b
Aaron Markey <markeyaaron@gmail.com>
2016-10-12 20:09:51 -0500

    Initial Commit

---
+++ Swiftility/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Swiftility
+//
+//  Created by Aaron Markey on 10/12/16.
+//  Copyright © 2016 Aaron Markey. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
